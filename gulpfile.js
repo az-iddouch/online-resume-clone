095e551e4d1ffc0612b04f6a01a11196011cc9b4
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-09-27 22:53:59 +0100

    updated the build process to include minification and image compression + auto prefixing ...

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,89 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const useref = require("gulp-useref");
+const uglify = require("gulp-uglify");
+const gulpIf = require("gulp-if");
+const cssnano = require("gulp-cssnano");
+const imagemin = require("gulp-imagemin");
+// const cache = require("gulp-cache");
+const del = require("del");
+const runSequence = require("run-sequence");
 
-//Compile sass
-gulp.task('sass', function(){
-    return gulp.src(['src/sass/main.scss'])
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "src"
+    }
+  });
 });
 
-//watch and serve
-gulp.task('serve', ['sass'] ,function (){
-    browserSync.init({
-        server: './src'
-    });
-
-    gulp.watch(['src/sass/**/*.scss'], ['sass']);
-    gulp.watch(['src/*.html']).on('change', browserSync.reload);
+gulp.task("sass", function() {
+  return gulp
+    .src("src/scss/**/*.scss") // Gets all files ending with .scss in src/scss
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 4 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("src/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
-gulp.task('default', ['serve']);
+gulp.task("useref", function() {
+  return (
+    gulp
+      .src("src/*.html")
+      .pipe(useref())
+      .pipe(gulpIf("*.js", uglify()))
+      // Minifies only if it's a CSS file
+      .pipe(gulpIf("*.css", cssnano()))
+      .pipe(gulp.dest("dist"))
+  );
+});
+
+// Compress all images and move them to /dist/images
+gulp.task("images", function() {
+  return (
+    gulp
+      .src("src/images/**/*.+(png|jpg|jpeg|gif|svg)")
+      // Caching images that ran through imagemin
+      .pipe(
+        imagemin({
+          interlaced: true
+        })
+      )
+      .pipe(gulp.dest("dist/images"))
+  );
+});
+
+// Moves all fonts into /dist folder
+// gulp.task("fonts", function() {
+//   return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+// });
+
+// Gulp will delete the `dist` folder for you whenever gulp clean:dist is run.
+gulp.task("clean:dist", function() {
+  return del.sync("dist");
+});
+
+gulp.task("watch", ["browser-sync", "sass"], function() {
+  gulp.watch("src/scss/**/*.scss", ["sass"]);
+  gulp.watch("src/*.html", browserSync.reload);
+  gulp.watch("src/js/**/*.js", browserSync.reload);
+});
+
+gulp.task("build", function(callback) {
+  runSequence("clean:dist", ["sass", "useref", "images"], callback);
+});
+
+gulp.task("default", function(callback) {
+  runSequence(["sass", "browser-sync", "watch"], callback);
+});
